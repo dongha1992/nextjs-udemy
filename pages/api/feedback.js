dd361ad6a050b645efc3f288ce829693b29b9065
donghakim <donghakim@donghakimui-MacBookPro.local>
2022-02-13 19:52:32 +0900

    mongodb add

---
+++ pages/api/feedback.js
@@ -0,0 +1,14 @@
+import { MongoClient } from "mongodb";
+
+const apiClient = async (req, res) => {
+  // mongoDb
+
+  const client = await MongoClient.connect("key").then((client) => {
+    const db = client.db();
+    await db.collection("emails").insertOne({ email: userEmail });
+    client.close();
+    res.status(200).json({ message: "success" });
+  });
+};
+
+export default apiClient;
